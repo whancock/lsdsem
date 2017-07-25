eb2d95a5b8c5fa5cd436ebac971cbe9f44337241
Will Hancock <will@willhancock.net>
2017-07-25 13:09:11 +0900

    more refactor

--- src/lsdsem.py
+++ src/lsdsem.py
@@ -10,46 +10,46 @@ from dataset import RocDataset
 from embedding import WVEmbedding
 
 
-class Moo:
 
-    def __init__(self):
+def run():
 
-
-        np.random.seed(1234)
-        tf.set_random_seed(1234)
+    np.random.seed(1234)
+    tf.set_random_seed(1234)
 
 
-        # setup a logger
-        logger = logging.getLogger('neural_network')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # setup a logger
+    logger = logging.getLogger('neural_network')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        handler_stdout = logging.StreamHandler(sys.stdout)
-        handler_stdout.setFormatter(formatter)
+    handler_stdout = logging.StreamHandler(sys.stdout)
+    handler_stdout.setFormatter(formatter)
 
-        logger.addHandler(handler_stdout)
-        logger.setLevel(logging.INFO)
+    logger.addHandler(handler_stdout)
+    logger.setLevel(logging.INFO)
 
 
 
-        logger.info('Loading data')
-        data = RocDataset()
-        logger.info('Loading embeddings')
-        embedding = WVEmbedding(data)
+    logger.info('Loading data')
+    data = RocDataset()
+    logger.info('Loading embeddings')
+    embedding = WVEmbedding(data)
 
 
 
-        train_examples = data.get_good_bad_split(embedding, data.train_data)
-        dev_examples = data.get_dev_repr(embedding, data.dev_data)
-        test_examples = data.get_dev_repr(embedding, data.test_data)
+    train_examples = data.get_good_bad_split(embedding, data.train_data)
+    dev_examples = data.get_dev_repr(embedding, data.dev_data)
+    test_examples = data.get_dev_repr(embedding, data.test_data)
 
 
-        model = LSDModel(data, embedding)
-        governor = Governor(logger, model)
+    model = LSDModel(data, embedding)
+    governor = Governor(logger, model)
 
-        governor.train_model(train_examples, dev_examples)
-        # governor.test_model(test_examples)
+    # governor.train_model(train_examples, dev_examples)
+    governor.test_model(test_examples)
 
 
 
 
-Moo()
+if __name__ == "__main__":
+    run()
+
